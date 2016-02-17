9a9628b5883e396f8c7a3ecb408784a4789de80d
muybienw <mubin.w@gmail.com>
2016-02-17 22:51:29 +0000

    added time tracking code

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,10 +10,24 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+
+	// Feature detects Navigation Timing API support.
+	if (window.performance) {
+		// Sends the hit, passing `performance.now()` as the timing value.
+		ga('send', 'timing', 'JS Dependencies', 'load', performance.now());
+	}
+
+
 	$('.likeBtn').click(function(e){
 		e.preventDefault();
 		console.log("like button clicked!");
 
 		ga("send", "event", 'like', 'click');
+
+		// Feature detects Navigation Timing API support.
+		if (window.performance) {
+			// Sends the hit, passing `performance.now()` as the timing value.
+			ga('send', 'timing', 'JS Dependencies', 'hit like btn', performance.now());
+		}
 	});
 }
